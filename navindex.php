e7600b3a902f8bb81a1a34dc5bbb63563f1672d8
Vincent Ygbuhay <67428784+G-web007@users.noreply.github.com>
2023-03-09 11:02:59 +0800

    Add files via upload

---
+++ navindex.php
@@ -0,0 +1,17 @@
+<div style="height:5px; background:rgb(6, 14, 32)"></div>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><strong>C.R.U.D</strong></a>
+            <button class="navbar-toggler mb-2" type="button" data-bs-toggle="collapse" data-bs-target="#dataTarget">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="dataTarget">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item mx-2">
+                        <a class="nav-link btn btn-success text-white" href="addSection.php" type="button">Add Section</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<div style="height:5px; background:rgb(6, 14, 32)"></div>
